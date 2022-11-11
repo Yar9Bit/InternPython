dbb80d47503fe432cdc5f264e3afce031170b110
yar9bit <yar9bit@gmail.com>
2022-11-11 10:32:57 +0500

    1

---
+++ task1/score.py
@@ -0,0 +1,66 @@
+from pprint import pprint
+import random
+import math
+
+TIMESTAMPS_COUNT = 50000
+
+PROBABILITY_SCORE_CHANGED = 0.0001
+
+PROBABILITY_HOME_SCORE = 0.45
+
+OFFSET_MAX_STEP = 3
+
+INITIAL_STAMP = {
+    "offset": 0,
+    "score": {
+        "home": 0,
+        "away": 0
+    }
+}
+
+
+def generate_stamp(previous_value):
+    score_changed = random.random() > 1 - PROBABILITY_SCORE_CHANGED
+    home_score_change = 1 if score_changed and random.random() > 1 - \
+                             PROBABILITY_HOME_SCORE else 0
+    away_score_change = 1 if score_changed and not home_score_change else 0
+    offset_change = math.floor(random.random() * OFFSET_MAX_STEP) + 1
+
+    return {
+        "offset": previous_value["offset"] + offset_change,
+        "score": {
+            "home": previous_value["score"]["home"] + home_score_change,
+            "away": previous_value["score"]["away"] + away_score_change
+        }
+    }
+
+
+def generate_game():
+    stamps = [INITIAL_STAMP, ]
+    current_stamp = INITIAL_STAMP
+    for _ in range(TIMESTAMPS_COUNT):
+        current_stamp = generate_stamp(current_stamp)
+        stamps.append(current_stamp)
+
+    return stamps
+
+
+game_stamps = generate_game()
+
+
+# pprint(game_stamps)
+
+
+def get_score(game_stamps, offset):
+    away = game_stamps[offset].get('score').get('away')
+    home = game_stamps[offset].get('score').get('home')
+    offset = game_stamps[offset].get('offset')
+    if home | away == 0:
+        return 'Нет очков команд'
+    else:
+        # for _ in game_stamps:
+        #   print(_)
+        return f'Счет команд при offset равным {offset}', home, away
+
+
+pprint(get_score(game_stamps, offset=TIMESTAMPS_COUNT))
