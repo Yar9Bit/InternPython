ffd9eb68a1b1878b83a6ed8d6ad7a7e768fe46a8
yar9bit <yar9bit@gmail.com>
2022-11-28 21:58:26 +0500

    Update TestScore.py

--- task2/TestScore.py
+++ task2/TestScore.py
@@ -1,18 +1,20 @@
-from unittest import TestCase, main, skip
+from unittest import TestCase, main
 
 from task1.score import generate_game, get_score
 
 
 class TestScore(TestCase):
-    game = generate_game()
-    stamps = [{'offset': 0, 'score': {'home': 0, 'away': 0}},
-              {'offset': 3, 'score': {'home': 1, 'away': 0}},
-              {'offset': 6, 'score': {'home': 1, 'away': 1}},
-              {'offset': 9, 'score': {'home': 1, 'away': 2}},
-              {'offset': 12, 'score': {'home': 2, 'away': 2}},
-              {'offset': 15, 'score': {'home': 3, 'away': 2}},
-              {'offset': 18, 'score': {'home': 3, 'away': 2}},
-              {'offset': 21, 'score': {'home': 3, 'away': 3}}]
+
+    def setUp(self) -> None:
+        self.game = generate_game()
+        self.stamps = [{'offset': 0, 'score': {'home': 0, 'away': 0}},
+                       {'offset': 3, 'score': {'home': 1, 'away': 0}},
+                       {'offset': 6, 'score': {'home': 1, 'away': 1}},
+                       {'offset': 9, 'score': {'home': 1, 'away': 2}},
+                       {'offset': 12, 'score': {'home': 2, 'away': 2}},
+                       {'offset': 15, 'score': {'home': 3, 'away': 2}},
+                       {'offset': 18, 'score': {'home': 3, 'away': 2}},
+                       {'offset': 21, 'score': {'home': 3, 'away': 3}}]
 
     def test_start_offset_function(self):
         self.assertEqual(get_score(self.game, 0), (0, 0))
